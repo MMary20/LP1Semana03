d2ba032171e5d8dc5a62260c4e50ba070f4e67b0
Maria Margarida <mmargarida688rgmail.com>
2025-03-07 11:44:39 +0000

    Added project RightSize to solution LP1Semana03.

---
+++ RightSize/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class RightSize
+{
+    static void Main(string[] entradas)
+    {
+        foreach (string entrada in entradas)
+        {
+            if (entrada.Length >= 8)
+            {
+                Console.WriteLine("[EARLY STOP]");
+                return;
+            }
+            if (entrada.Length > 3)
+            {
+                Console.WriteLine(entrada);
+            }
+        }
+    }
+}
+
