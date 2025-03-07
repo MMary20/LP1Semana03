30e075912c7684c2859e5b7f45839139b6d94881
Maria Margarida <mmargarida688rgmail.com>
2025-03-07 12:12:33 +0000

    Added ChangeString project to solution LP1Semana03.

---
+++ ChangeString/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ChangeString
+{
+    static void Main()
+    {
+        Console.Write("String: ");
+        string texto = Console.ReadLine();
+        
+        Console.Write("Caráter: ");
+        char carParaSubstituir = Console.ReadLine()[0]; 
+
+        string resultado = texto.Replace(carParaSubstituir, 'x');
+
+        Console.WriteLine("Será impresso: " + resultado);
+    }
+}
+
