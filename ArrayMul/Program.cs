02ffa1470422ca3345d60b5808b697dfbd382655
Maria Margarida <mmargarida688rgmail.com>
2025-03-07 12:54:41 +0000

    Added project ArrayMul to solution LP1Semana03.

---
+++ ArrayMul/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class ArrayMul
+{
+    static void Main(string[] args)
+    {
+        float[,] A = {
+            { float.Parse(args[0]), float.Parse(args[1]) },
+            { float.Parse(args[2]), float.Parse(args[3]) }
+        };
+        float[] B = { float.Parse(args[4]), float.Parse(args[5]) };
+
+        for (int i = 0; i < 2; i++)
+        {
+            float resultado = 0;
+            for (int j = 0; j < 2; j++)
+            {
+                resultado += A[i, j] * B[j];
+            }
+
+            Console.WriteLine($"| {resultado,7:F2} |");
+        }
+    }
+}
+
+
