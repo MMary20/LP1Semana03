d3623df8167b4591a064d5d1f633e17e040dbdbf
Maria Margarida <mmargarida688rgmail.com>
2025-03-07 13:10:50 +0000

    Added project Pow2Until to solution LP1Semana03.

---
+++ Pow2Until/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Pow2Until
+{
+    static void Main(string[] args)
+    {
+        PowersOf2Until5();
+        PowersOf2Until5();
+    }
+    private static void PowersOf2Until5()
+    {
+        int numero = 1;
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.WriteLine(numero);
+            numero *= 2;  
+        }
+    }
+}
+
+
